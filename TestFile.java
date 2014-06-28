ed2d4216003da4ed0657cb66845a86b3305b7bdf
clopd <patrick.j.lennon@gmail.com>
2014-06-28 22:56:00 +0100

    Create TestFile.java

---
+++ TestFile.java
@@ -0,0 +1,18 @@
+
+public class JavaTutorial1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Big Dog Was here");
+		int age;
+		age=33;
+		System.out.println("John's age today is "+age);
+		age += 3;
+		System.out.println("In 3 Years John's age is "+age);
+		System.out.println("10 plus 3 is = " +30 / 3);
+		System.out.println("10 div 3 is " + 10/3);
+		System.out.println("10 % of 3 is " + 10%3);
+	}
+
+}
